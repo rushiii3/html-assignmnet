89f8a9f32c89e4da8009b1f3046a1596d4b361c2
rushiii3 <105168088+rushiii3@users.noreply.github.com>
2022-05-19 12:30:59 +0530

    Add files via upload

---
+++ rh.php
@@ -0,0 +1,11 @@
+<!Doctype Html>  
+<Html>     
+<Head>      
+<Title>     
+Use a Php in Html  
+</Title>  
+</Head>  
+<Body>   
+ <h1><?php echo "Hii User!! You are at JavaTpoint Site" ?></h1>  
+</Body>   
+</Html>  
